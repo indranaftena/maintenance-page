ac578164f5ca22f7e0fd98883f00557803508c25
indranaftena <indranaftena@gmail.com>
2023-03-14 12:44:22 +0700

    add button animation, favicon, clean code

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>This page is under maintenance</h1>
-    <p class="read-the-docs">
+    <p class="message">
       sorry for the inconvenience
     </p>
     <div class="card">
-      <button id="counter" type="button">Try click me</button>
+      <div id="fancy"></div>
+      <button id="counter" type="button">Change color</button>
     </div>
   </div>
 `
